d05b0caed4242157e4a530a2b1c72d079e37b6b5
Tony S <tsallustro@outlook.com>
2025-08-07 17:34:29 -0400

    Character data

--- Assets/Scripts/CharacterData/LevelCharacterData.cs
+++ Assets/Scripts/CharacterData/LevelCharacterData.cs
@@ -5,6 +5,7 @@ public class LevelCharacterData : MonoBehaviour
     public static LevelCharacterData Instance;
 
     public CharacterData characterData;
+    public int currentHP = 1;
 
     private void Awake()
     {
@@ -18,5 +19,11 @@ public class LevelCharacterData : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetCharacterData(CharacterData newData)
+    {
+        this.characterData = newData;
+        currentHP = newData.HP;
     }
 }
